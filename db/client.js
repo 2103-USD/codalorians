c050970359958c250b3760e594368ca1abf3d75d
Anthony Fernan <anthonyfernan@icloud.com>
2021-08-18 17:47:47 -0700

    created PRODUCTS backend database adapters

---
+++ db/client.js
@@ -0,0 +1,10 @@
+// Connect to DB
+const { Client } = require('pg');
+
+const CONGO = 'shop-db'
+
+const DB_URL = process.env.DATABASE_URL || `postgres://localhost:5000/${ CONGO }`;
+const client = new Client(DB_URL);
+
+// export
+module.exports = {client}
