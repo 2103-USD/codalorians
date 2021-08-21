215041f8f4906249ea579c278c7788e1935e7396
Enzi <enzischow@yahoo.comu>
2021-08-21 13:48:33 -0700

    Added env to gitignore

--- db/order_products.js
+++ db/order_products.js
@@ -1,12 +1,50 @@
 const client = require("./client");
 
-async function getOrderProductById() {}
+async function getOrderProductById(id) {
+  try {
+    const {
+      rows: [orderProduct],
+    } = await client.query(
+      `
+            SELECT *
+            FROM order_products
+            WHERE id=$1;
+        `,
+      [id]
+    );
+    return orderProduct;
+  } catch (error) {
+    throw error;
+  }
+}
 
-async function addProductToOrder() {}
+//still needs work before this function is completed
+async function addProductToOrder({ orderId, routineId, price, quantity }) {
+  try {
+  } catch (error) {
+    throw error;
+  }
+}
 
 async function updateOrderProduct() {}
 
-async function destroyOrderProduct() {}
+async function destroyOrderProduct(id) {
+  try {
+    const {
+      rows: [orderProduct],
+    } = await client.query(
+      `
+            DELETE FROM order_products
+            WHERE id=$1
+            RETURNING *;
+        `,
+      [id]
+    );
+    return orderProduct;
+  } catch (error) {
+    throw error;
+  }
+}
 
 module.exports = {
   getOrderProductById,
