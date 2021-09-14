c6d0efd09f5ebf9b389a4c7b158aee4d0f61b098
alex-yambao <alx.yambao@gmail.com>
2021-09-13 20:09:05 -0700

    fixed image names

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { Nav, Container, Button } from "react-bootstrap";
-import userImg from "./images/User.png";
+import userImg from "./images/user.png";
 
 const Sidebar = (props) => {
   const { showSideBar, toggleSideBar, currentUser } = props;
